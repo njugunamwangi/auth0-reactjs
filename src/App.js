4cbc5b38d070f7f18d87c1a8af0d83b441c75fea
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-21 12:39:41 +0300

    Protect admin panel

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import Home from "./views/Home";
 import Users from "./views/Users";
-import {useContext} from "react";
-import {Auth0Context} from "./views/contexts/Auth0Context";
+import {useAuth0} from "./views/contexts/Auth0Context";
 import NotFound from "./views/core/NotFound";
 
 export default function App() {
 
-    const auth0 = useContext(Auth0Context)
-
+    const auth0 = useAuth0()
     console.log(auth0)
-
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/users" element={<Users />} />
                 <Route path="*" element={<NotFound />} />
+                { auth0.isAuthenticated && auth0.user && (
+                    <>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/users" element={<Users />} />
+                    </>
+                ) }
             </Routes>
         </BrowserRouter>
     );
